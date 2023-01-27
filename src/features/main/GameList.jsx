0fd35e5da7f35772a7399c02f857c3e8c0ed26d0
Ju <97508841+haruharuganda@gmail.com>
2023-01-27 22:28:53 +0900

    head 1차 로그인/비로그인 구분

--- src/features/main/GameList.jsx
+++ src/features/main/GameList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const GameList = () => {
-  return <div></div>;
+  return <div>안녕하세요</div>;
 };
 
 export default GameList;
--- src/reduex/modules/loginSlice.js
+++ src/reduex/modules/loginSlice.js
@@ -12,8 +12,14 @@ export const __postLogin = createAsyncThunk(
   async ({ user, navigate }, thunkAPI) => {
     try {
       const data = await axiosInstance.post(`user/login`, user);
+      if (data.data.statuscode === 200) {
+        const token = data.headers.get("Authorization");
+        localStorage.setItem("token", token);
+        navigate("/");
+      } else {
+        alert("문제가 생겼습니다.");
+      }
 
-      navigate("/");
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       const errorObject = error.response.data;
