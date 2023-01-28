b0676d312c23d2f5a0dabf48cf2a73b715ea071d
Ju <97508841+haruharuganda@gmail.com>
2023-01-28 14:14:17 +0900

    Cards나머지커밋

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Router from "./page/Router";
-
+import "./styles/font.css";
 function App() {
   return <Router />;
 }
--- src/reduex/config/store.js
+++ src/reduex/config/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "../modules/loginSlice";
 import SignupSilce from "../modules/SignupSilce";
-import gameInfoSlice from "../modules/gameInfoSlice";
+import mainGameInfoSlice from "../modules/mainGameInfoSlice";
 const store = configureStore({
-  reducer: { loginSlice, SignupSilce, gameInfoSlice },
+  reducer: { loginSlice, SignupSilce, mainGameInfoSlice },
 });
 
 export default store;
